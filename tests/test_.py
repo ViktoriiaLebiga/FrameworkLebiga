60afc60a6677e0bb359f0250fc4cf757f235994a
Viktoriia <vikusia.lebiga@gmail.com>
2018-08-06 15:36:36 +0300

    commit asser

--- tests/test_.py
+++ tests/test_.py
@@ -8,18 +8,20 @@ empty_message = 'The "E-mail" field is required.'
 invalid_message = 'The "E-mail" field must contain a valid email address.'
 
 def test_empty_fild_message(driver):
-   driver.get("https://www.vpnunlimitedapp.com/en")
-   email_fild = driver.find_element_by_xpath("//div[@class='footer_input_wrap']/input[@class='footer_input_form']")
+   try:
+      driver.get("https://www.vpnunlimitedapp.com/en")
+      email_fild = driver.find_element_by_xpath("//div[@class='footer_input_wrap']/input[@class='footer_input_form']")
 
-   email_fild.send_keys(empty)
-   btn = driver.find_element_by_xpath("//div[@class='footer_input_wrap']/label[@class='footer_input_btn_label']")
-   btn.click()
-   message = driver.find_element_by_xpath("//p[@class='form-error']").text
-   wait(driver)
+      email_fild.send_keys(empty)
+      btn = driver.find_element_by_xpath("//div[@class='footer_input_wrap']/label[@class='footer_input_btn_label']")
+      btn.click()
+      message = driver.find_element_by_xpath("//p[@class='form-error']").text
+      wait(driver)
 
-   assert message == empty_message
-   with allure.MASTER_HELPER.step('Error'):
-      allure.MASTER_HELPER.attach('screen_shot', driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+      assert message == empty_message
+   finally:
+      with allure.MASTER_HELPER.step('Error'):
+         allure.MASTER_HELPER.attach('screen_shot', driver.get_screenshot_as_png(), type=AttachmentType.PNG)
 
    print('User cannot receive email. Show valid error message')
 
